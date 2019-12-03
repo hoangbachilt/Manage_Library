9b2b806fedbb5f9fe37fcca656e4c8b19af06def
LeHongDuc274 <Lehongduc274@gmail.com>
2019-12-04 02:47:28 +0700

    US010_follow_books

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,3 @@ category = Category.create! title: "Toan"
 10.times.each{|x| Category.create( title: "Toan#{x}")}
 
 
-
---
+++ test/controllers/follow_books_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FollowBooksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
